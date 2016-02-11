c526d217e9795cf8d193e9b3224b3ebf195e5198
sofienystrom <sofienystrom@MacBook-Air.local>
2016-02-11 21:41:31 +0100

    started on JS

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -3,25 +3,37 @@ var DinnerModel = function() {
  
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
-
+    this.selectedDishes = 0;
+    this.numberOfGuests = 0;
 
 	this.setNumberOfGuests = function(num) {
 		//TODO Lab 2
+        this.numberOfGuests = num;
 	}
 
 	// should return 
 	this.getNumberOfGuests = function() {
 		//TODO Lab 2
+        return this.numberOfGuests;
 	}
 
 	//Returns the dish that is on the menu for selected type 
 	this.getSelectedDish = function(type) {
 		//TODO Lab 2
+        for (dish in this.selectedDishes) {
+            if(type === dish.type) {
+                return dish.name;
+            }
+        }
 	}
 
 	//Returns all the dishes on the menu.
 	this.getFullMenu = function() {
 		//TODO Lab 2
+        for (dish in dishes) {
+            
+        }
+        
 	}
 
 	//Returns all ingredients for all the dishes on the menu.
