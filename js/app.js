13d2daa9f9914097f410a5d1718d92805bb42d0b
Sofie <sofie@tii.se>
2016-02-16 16:20:53 +0100

    added views

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(function() {
 	
 	//And create the needed controllers and views
 	//var exampleView = new SingleDishView($("#view"), model);
-    var exampleView = new PrintView($("#view"), model);
+    //var exampleView = new PrintView($("#view"), model);
+    var exampleView = new indexView($("#view"), model);
 
 });
