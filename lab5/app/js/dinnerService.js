7d96e5110ebbbe283503b4f27ffb8dc1b8b8787d
Sofie <sofie@tii.se>
2016-03-15 20:21:02 +0100

    last update lab5

--- lab5/app/js/dinnerService.js
+++ lab5/app/js/dinnerService.js
@@ -3,20 +3,53 @@
 // dependency on any service you need. Angular will insure that the
 // service is created first time it is needed and then just reuse it
 // the next time.
+
 dinnerPlannerApp.factory('Dinner',function ($resource,$cookieStore) {
+
 
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
   // you will need to modify the model (getDish and getAllDishes) 
   // a bit to take the advantage of Angular resource service
   // check lab 5 instructions for details
+  
+    var self = this;
+    this.SingleDish;
+    this.selectedDishes = [];
+    
+    
+    this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:'H9n1zb6es492fj87OxDtZM9s5sb29rW3'});
+    
+    this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:'H9n1zb6es492fj87OxDtZM9s5sb29rW3'});
+    
+    
+    this.getDish = function(Dishid){
+        
+        self.SingleDish = self.Dish.get({id:Dishid})
+        return self.SingleDish;
+    }
+    
+    for (i in $cookieStore.get('selectedDishes')) {
+        console.log("FETCHING DISHES");
+        var id = $cookieStore.get('selectedDishes')[i];
+        this.selectedDishes.push(self.getDish(id));
+    }
+   
+    if($cookieStore.get("numberOfGuests")) {
+        this.numberOfGuests = $cookieStore.get("numberOfGuests");
+        console.log("number of guests:" + $cookieStore.get("numberOfGuests"));
+    }
+    else {
+        this.numberOfGuests = 1;
+    }
+    
+    
+    this.numberOfGuests = $cookieStore.get('numberOfGuests');
 
-    this.selectedDishes=[]; // should be set by cookies
     
 	this.setNumberOfGuests = function(num) {
         
         this.numberOfGuests = num;
-        
         
         $cookieStore.put('numberOfGuest', num);
     
@@ -71,34 +104,49 @@ dinnerPlannerApp.factory('Dinner',function ($resource,$cookieStore) {
                 
             }
         }
-        console.log(totalprice);
+        
         return totalprice;
 	}
 
 	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
 	//it is removed from the menu and the new one added. (getSelectedDishes work here)
 
+    
 	this.addDishToMenu = function() {
+        var array = []; //array of id
+        
 		var length = this.selectedDishes.length;
-        //var index = this.selectedDishes.indexOf(self.SingleDish);
         var SeDishes = this.selectedDishes;
+        console.log(this.selectedDishes);
+        //var index = this.selectedDishes.indexOf(self.SingleDish);
         //TODO Lab 2 
         for (i=0; i<length;i++) {
-
+            console.log(SeDishes[i].Category);
             if(SeDishes[i].Category === self.SingleDish.Category) {
                 SeDishes.splice(i,1);
             }; 
             
         };
-        SeDishes.push(self.SingleDish);
         self.selectedDishes = SeDishes;
         
+        for (i in self.selectedDishes) {
+            array.push(self.selectedDishes[i].RecipeID);
+        };
+        
+        self.selectedDishes.push(self.SingleDish);
+        array.push(self.SingleDish.RecipeID);
+        
+        $cookieStore.put("selectedDishes", array);
+        
+        console.log(array);
+    
 	}
 	//Removes dish from menu
 	this.removeDishFromMenu = function(id) {
 
         var index = this.selectedDishes.indexOf(self.SingleDish);
         this.selectedDishes.splice(index,1);
+        $cookieStore.remove(id);
        
 	}
     
@@ -106,18 +154,8 @@ dinnerPlannerApp.factory('Dinner',function ($resource,$cookieStore) {
 	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
 	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
 	//if you don't pass any filter all the dishes will be returned
-    
-    this.DishSearch = $resource('http://api.bigoven.com/recipes',{pg:1,rpp:25,api_key:'18f3cT02U9f6yRl3OKDpP8NA537kxYKu'});
-    
-    this.Dish = $resource('http://api.bigoven.com/recipe/:id',{api_key:'18f3cT02U9f6yRl3OKDpP8NA537kxYKu'}); 
-    
-    var self = this;
-    this.SingleDish;
-    this.getDish = function(Dishid){
-        
-        self.SingleDish = self.Dish.get({id:Dishid})
-        return self.SingleDish;
-    }
+ 
+
     
     this.getDishWithName = function(name){
         for(key in this.selectedDishes){
